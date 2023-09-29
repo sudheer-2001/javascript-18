97d04f4ce71b941cee66a60b2fb322883d330616
Sudheer Boppana <104917342+sudheer-2001@users.noreply.github.com>
2023-09-29 07:07:18 +0000

    datatypes-summary

---
+++ 01/datatypes-summary.js
@@ -0,0 +1,66 @@
+// Datatypes - 2 types
+// primitive
+// Reference (Non-primitive)
+
+//Primitive=> 7 types: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+// Null -> empty,
+// Undefined -> variable declared,memory space declared , but no value declared
+// Symbol -> unique
+// BigInt -> require large numbers where it will useful
+
+const score = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail; // by manually => let userEmail = undefined;
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+// console.log(id === anotherId); => false
+
+//javascript internally treated - particular datatype.
+const bigNumber = 987549837534743543n 
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
+const heroes = ["prabhas", "mahesh", "ashwin"]
+
+let myObj = {
+    name: "sudheer",
+    age: 22
+}
+
+// function is like a variable
+const myFunction = function(){
+    console.log("hello world!")
+}
+
+// console.log(typeof anotherId) => symbol
+// console.log(typeof bigNumber) => bigint
+// console.log(typeof sudheer) => undefined --> due to there is no sudheer variable
+// console.log(typeof outsideTemp) => object --> null is a type of object
+
+// console.log(typeof heroes) => object
+// console.log(typeof myObj) => object
+// console.log(typeof myFunction) => function -> basically it is an object function
+
+//******************************************** 
+// Stack (Primitive) memory (value type assignment)   
+// & Heap (Non-Primitive) memory (reference type assignment)
+
+let myName = "Hello"
+let anotherName = "Hiii"
+anotherName = "Wonderful"
+
+// console.log(myName) => Hello
+// console.log(anotherName) => Wonderful
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+let userTwo = userOne
+userTwo.email = "sudheer@google.com"
+
+// console.log(userOne.email) => sudheer@google.com
+// console.log(userTwo.email); => sudheer@google.com
